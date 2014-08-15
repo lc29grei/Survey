bda67364e1449bff510c19d685180d9b62a5745d
lc29grei <lc29grei@siena.edu>
2014-08-15 13:29:07 -0400

    jps update

--- src/com/axa/servlets/SurveyAppServlet.java
+++ src/com/axa/servlets/SurveyAppServlet.java
@@ -27,8 +27,7 @@ public class SurveyAppServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		PrintWriter out = response.getWriter();
-        out.println("Update");
+		request.setAttribute("message", "hello");
 	}
 
 	/**
