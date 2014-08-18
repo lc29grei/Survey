e755c90134856772cea398a4fbe244f420394449
lc29grei <lc29grei@siena.edu>
2014-08-18 09:27:36 -0400

    test sync

--- jsp/index.jsp
+++ jsp/index.jsp
@@ -1,4 +1,13 @@
-<body>
-	<jsp:include page="/latest_products.jsp" />
-    <c:out value="${message }"></c:out>
-</body>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"  
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" 
+	"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+		<title> My first JSP   </title>
+	</head>	
+	<body>		
+		<p>The result is ${result}</p>
+	</body>	
+</html>
--- src/com/axa/servlets/SurveyAppServlet.java
+++ src/com/axa/servlets/SurveyAppServlet.java
@@ -3,6 +3,7 @@ package com.axa.servlets;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -25,10 +26,13 @@ public class SurveyAppServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		request.setAttribute("message", "hello");
-	}
+    protected void doGet(HttpServletRequest request, 
+    	      HttpServletResponse response) throws ServletException, IOException 
+    	  {
+    	request.setAttribute("result", "This is the result of the servlet call");
+    	request.getRequestDispatcher("/index.jsp").forward(request, response);
+    	  }
+    	
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
